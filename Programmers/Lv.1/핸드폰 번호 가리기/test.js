ae68205a426ed6c6a057f97ef1e31ae1ef0bd793
shjoo <shjoo@intube.kr>
2024-06-27 09:56:15 +0900

    24.06.27 코딩테스트

---
+++ Programmers/Lv.1/핸드폰 번호 가리기/test.js
@@ -0,0 +1,33 @@
+function solution(phone_number) {
+    let phone = phone_number.split('');
+
+    for(let i=0;i<phone.length-4;i++){
+        phone[i]='*'
+    }
+
+    return phone.join('')
+}
+
+console.log(solution("01033334444"));
+console.log(solution("027778888"));
+
+// 다른 사람 풀이 1
+
+function hide_numbers(s){
+    return s.replace(/\d(?=\d{4})/g, "*");
+}
+
+console.log("결과 : " + hide_numbers('01033334444'));
+
+// 다른 사람 풀이 2
+
+function hide_numbers(s){
+    var result = "*".repeat(s.length - 4) + s.slice(-4);
+    return result;
+}
+
+console.log("결과 : " + hide_numbers('01033334444'));
+
+  
+// 다른 사람 풀이 3
+const solution = n => [...n].fill("*",0,n.length-4).join("")
