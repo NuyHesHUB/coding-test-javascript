4d7124a32bb0d918b1f7af3463608c34bc0527d0
NuyHesHUB <7upbeat7@naver.com>
2025-01-06 23:52:08 +0900

    25.01.06 내적 풀이

--- Programmers/Lv.1/내적/test.js
+++ Programmers/Lv.1/내적/test.js
@@ -1,7 +1,31 @@
 function solution(a, b) {
-    var answer = 1234567890;
-    return answer;
-}
+    let result = [];
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+        result.push(a[i] * b[i]);
+    }
+    result.forEach((item) => {
+        sum += item
+    }) 
+    
+    
+    return sum;
+} 
 
-console.log(solution('1'));
-console.log(solution());
+function solution(a, b) {
+    let result = 0;
+    for (let i = 0; i < a.length; i++) {
+        result += a[i] * b[i];
+    }
+    
+    return result;
+} 
+
+function solution(a, b) {
+    return a.reduce((acc, cur, idx) => {
+            return acc + cur * b[idx];
+        }, 0);
+} 
+
+console.log(solution([1,2,3,4], [-3,-1,0,2]));
+console.log(solution([1,2,3], [4,5,6]));
