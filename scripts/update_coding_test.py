7bae3dcf26a7d762f2058bf9b96ef38e516d7271
NuyHesHUB <shjoo@intube.kr>
2025-01-08 15:04:00 +0900

    test10

--- scripts/update_coding_test.py
+++ scripts/update_coding_test.py
@@ -10,35 +10,22 @@ README_PATH = os.path.join(repo_path, 'README.md')
 REPO_URL = 'https://github.com/NuyHesHUB/coding-test-javascript/tree/main'
 
 def get_latest_file_path():
+    repo = git.Repo(repo_path)  # 'repo_path' 경로의 Git 리포지토리를 엽니다.
+    latest_commit = repo.head.commit  # 현재 브랜치의 최신 커밋을 가져옵니다.
+
     try:
-        repo = git.Repo(repo_path)  # 'repo_path' 경로의 Git 리포지토리를 엽니다.
-        commits = list(repo.iter_commits('HEAD'))  # 현재 브랜치의 모든 커밋을 가져옵니다.
-
-        if len(commits) == 0:
-            print("No commits in the repository.")
-            return None  # 커밋이 없으면 None 반환
-
-        latest_commit = commits[0]  # 최신 커밋
-
-        # 커밋이 하나만 있을 경우, 파일 목록을 직접 반환
-        if len(commits) == 1:
-            for file in latest_commit.stats.files.keys():
-                if file.endswith('.js'):
-                    return file
-            return None
-
-        # 최신 커밋과 이전 커밋 간의 차이를 확인
+        # 최신 커밋의 변경된 파일 목록을 가져옵니다.
         for diff in latest_commit.diff('HEAD~1'):
-            if diff.a_path.endswith('.js'):
-                return diff.a_path  # js 파일의 경로를 반환
+            if diff.a_path.endswith('.js'):  # 변경된 파일이 js 파일인지 확인합니다.
+                return diff.a_path  # js 파일의 경로를 반환합니다.
     except git.exc.GitCommandError as e:
         print(f"Git Command Error: {e}")
-        return None
-    except Exception as e:
-        print(f"Unexpected Error: {e}")
-        return None
-
-    return None  # 변경된 js 파일이 없으면 None 반환
+        return None  # 오류가 발생하면 None을 반환합니다.
+    except IndexError:
+        print("No previous commit found.")
+        return None  # 이전 커밋이 없으면 None을 반환합니다.
+    
+    return None  # js 파일이 없으면 None을 반환합니다.
     
 
 def get_new_entry(file_path):
