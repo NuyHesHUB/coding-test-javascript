f2b25ff88aef5666d43ae0e9f69127eaaa22acab
NuyHesHUB <7upbeat7@naver.com>
2025-01-13 23:49:05 +0900

    25.01.13 update2

--- Programmers/Lv.1/가운데 글자 가져오기/test.js
+++ Programmers/Lv.1/가운데 글자 가져오기/test.js
@@ -17,3 +17,31 @@ console.log(solution("qwer"));
 // 7 일 때 4번째 
 // 8 일 때 4,5번째 
 
+/* function solution(s) {
+    return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
+} */
+
+/* function solution(s) {
+    return s.length % 2 == 0 ? s.substr(s.length / 2 - 1, 2) : s.substr(Math.floor(s.length / 2), 1);
+} */
+
+/* function solution(s) {
+    var length = s.length;
+    var answer = '';
+
+    if(!(s.length >0 && s.length<100)) {
+        return;
+    }
+
+    if(length % 2 != 0) {
+        answer += s.slice(length/2, length/2 +1);
+    }else {
+        answer += s.slice(length/2 -1, length/2 +1);
+    }
+
+    return answer;
+
+}
+
+var s = 'qwerty';
+console.log(solution(s)); */
